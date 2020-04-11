c876f7633f8b483f04151cf83c0271ab89e71870
Jude <boblitejude@gmail.com>
2020-04-11 18:12:14 +0200

    challenge 1 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,25 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+	let currentlyInfectedI = data.reportedCases * 10;
+	let currentlyInfectedSevere = data.reportedCases * 50;
+	let factor = Math.floor(data.timeToElapse/3);
+	let infectionsByRequestedTimei = currentlyInfectedI * (Math.pow(2,factor));
+	let infectionsByRequestedTimeSevere = currentlyInfectedSevere * (Math.pow(2,factor))
+
+		let output = {
+			data: data,
+			impact: {
+				currentlyInfected: currentlyInfectedI,
+				infectionsByRequestedTime: infectionsByRequestedTimei,
+
+        },
+			severeImpact: {
+				currentlyInfected: currentlyInfectedSevere,
+				infectionsByRequestedTime: infectionsByRequestedTimeSevere,
+        }
+		}
+	return output;
+
+}
+
 
 export default covid19ImpactEstimator;
