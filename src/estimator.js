750c501536d3d8a721d444a737c2a2dcbb85cdf6
Jude <boblitejude@gmail.com>
2020-04-11 20:51:37 +0200

    challenge 1 errors correction rerun

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,23 @@
 const covid19ImpactEstimator = (data) => {
-    const currentlyInfectedI = data.reportedCases * 10;
-    const currentlyInfectedSevere = data.reportedCases * 50;
-    const factor = Math.floor(data.timeToElapse / 3);
-    const infectionsByRequestedTimei = currentlyInfectedI * (2 ** factor);
-    const infectionsByRequestedTimeSevere = currentlyInfectedSevere * (2 ** factor);
+  const currentlyInfectedI = data.reportedCases * 10;
+  const currentlyInfectedSevere = data.reportedCases * 50;
+  const factor = Math.floor(data.timeToElapse / 3);
+  const infectionsByRequestedTimei = currentlyInfectedI * (2 ** factor);
+  const infectionsByRequestedTimeSevere = currentlyInfectedSevere * (2 ** factor);
 
-    const output = {
-        data: data,
-        impact: {
-            currentlyInfected: currentlyInfectedI,
-            infectionsByRequestedTime: infectionsByRequestedTimei,
+  const output = {
+    data: data,
+    impact: {
+      currentlyInfected: currentlyInfectedI,
+      infectionsByRequestedTime: infectionsByRequestedTimei
 
-        },
-        severeImpact: {
-            currentlyInfected: currentlyInfectedSevere,
-            infectionsByRequestedTime: infectionsByRequestedTimeSevere,
-        }
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfectedSevere,
+      infectionsByRequestedTime: infectionsByRequestedTimeSevere
     }
-    return output;
+  }
+  return output;
 
 }
 
