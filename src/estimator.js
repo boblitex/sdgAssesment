ce8933f4ae023d589cffd2f0af7bd0793427d85a
Jude <boblitejude@gmail.com>
2020-04-12 17:03:23 +0200

    challenge 1 periodtype

--- src/estimator.js
+++ src/estimator.js
@@ -1,7 +1,7 @@
 const covid19ImpactEstimator = (data) => {
   const currentlyInfectedI = data.reportedCases * 10;
   const currentlyInfectedSevere = data.reportedCases * 50;
-  switch (data.timeToElapse) {
+  switch (data.periodType) {
     case "days":
       let factor = Math.floor(data.timeToElapse / 3);
       break;
