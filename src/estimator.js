9e289068eebe4aed93da0712188fab53d5f975da
Jude <boblitejude@gmail.com>
2020-04-12 04:20:23 +0200

    challenge 1 weeks and months

--- src/estimator.js
+++ src/estimator.js
@@ -1,7 +1,18 @@
 const covid19ImpactEstimator = (data) => {
   const currentlyInfectedI = data.reportedCases * 10;
   const currentlyInfectedSevere = data.reportedCases * 50;
-  const factor = Math.floor(data.timeToElapse / 3);
+  switch (data.timeToElapse) {
+    case "days":
+      let factor = Math.floor(data.timeToElapse / 3);
+      break;
+    case "weeks":
+      let factor = Math.floor((data.timeToElapse * 7) / 3);
+      break;
+    case "months":
+      let factor = Math.floor((data.timeToElapse * 30) / 3);
+      break;
+  }
+
   const infectionsByRequestedTimei = currentlyInfectedI * (2 ** factor);
   const infectionsByRequestedTimeSevere = currentlyInfectedSevere * (2 ** factor);
 
